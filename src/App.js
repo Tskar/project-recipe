50daccf2005ba2ca1513f9b6d5b2c09a3dd5e020
Tskar <sanskarthapaofficial@gmail.com>
2021-12-09 17:33:32 -0500

    Header update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello recipe-app!
+      <section className="title-bar">
+        <a href="#">find.recipe</a>
+      </section>
+
+      <section className="header-body">
+        <h1 className="header-info">
+          <span>The Hub To Find</span>
+          That Recipe
+        </h1>
+
+        <div className="search-container">
+          <div className="search-bar">
+            <input className="search-box" type="text" placeholder="dish,ingridient,keywords..." />
+            <p>Example:"Milkshake","Banana","Coffee"...</p>
+          </div>
+
+          <button className="search-button">Search Recipe</button>
+        </div>
+
+      </section>
+      
     </div>
   );
 }
